c4f55a211cf2406f0e761053ea1c1adff56ba298
Adam Kovacs <akova543@mtroyal.ca>
2023-11-16 22:20:55 -0700

    Fixed Victors Code

--- js/Assignment2.js
+++ js/Assignment2.js
@@ -5,3 +5,41 @@ const api = "http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
 const songlist = JSON.parse(songs);
 const genrelist = JSON.parse(genres);
 const artistlist = JSON.parse(artists);
+
+document.addEventListener("DOMContentLoaded", () => {
+    populateSongsTable();
+    populateSelect("#artist-select", artistlist);
+    populateSelect("#genre-select", genrelist);
+});
+
+function populateSongsTable(){
+    const table = document.querySelector("#song-table table tbody");
+    
+    songlist.forEach(song => {
+        const row = document.createElement("tr");
+        table.appendChild(row);
+
+        // Add song details directly
+        addCell(row, song.title);
+        addCell(row, song.artist.name);
+        addCell(row, song.year);
+        addCell(row, song.genre.name);
+        addCell(row, song.details.popularity);
+    });
+}
+
+function addCell(row, data){
+    const cell = document.createElement("td");
+    cell.textContent = data;
+    row.appendChild(cell);
+}
+
+function populateSelect(selector, list){
+    const select = document.querySelector(selector);
+    
+    list.forEach(item => {
+        const option = document.createElement("option");
+        option.textContent = item.name;
+        select.appendChild(option);
+    });
+}
