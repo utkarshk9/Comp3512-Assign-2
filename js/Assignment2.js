b5aa0b33f661dda321cc46f6827f22cd34027d2c
AlliVictor <valli978@mtroyal.ca>
2023-11-16 21:58:01 -0700

    reset, retry, errors were found

---
+++ js/Assignment2.js
@@ -0,0 +1,7 @@
+
+/* url of song api --- https versions hopefully a little later this semester */	
+const api = "http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php";
+
+const songlist = JSON.parse(songs);
+const genrelist = JSON.parse(genres);
+const artistlist = JSON.parse(artists);
